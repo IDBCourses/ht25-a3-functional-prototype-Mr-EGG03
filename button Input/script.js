35c4098c9c2e6ca3dfc0bcc4c5a3bcdfe2251f8c
Mr-EGG03 <geldeneg@gmail.com>
2025-10-31 16:00:03 +0100

    first atempt to do movement every 15 degrees

--- button Input/script.js
+++ button Input/script.js
@@ -7,129 +7,122 @@ import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
 let Input = [];
-let size = 50;
-let width = 100;
-let oriantation = 0;
-let startX = window.innerWidth / 2 - size / 2;
-let startY = window.innerHeight / 2- size / 2;
+let size = 100;
+let width = 50;
+let deg = 0;
+let currX = 0; 
+let currY = 0;
+let newX = 0; 
+let newY = 0; 
+let momentom = 0.01; 
 // Settings variables should contain all of the "fixed" parts of your programs
-const fullAhead = 5;
-const astern = 1;
-//other things 
-function forward() {//would it be better to only run this only when KeyY is present thus removing checking it evey if statment
- //Forward (default)
-  if (Input.includes("KeyY") && oriantation === 0){
-   startY -= fullAhead;
-  }
- // forward in bottom diagonal
-  if (Input.includes("KeyY") && (oriantation === 135 || oriantation === -225)){
-   startY += fullAhead;
-   startX += fullAhead;
-  }  
-  if (Input.includes("KeyY") && (oriantation === -135 || oriantation === 225)){
-   startY += fullAhead;
-   startX -= fullAhead;
-  }  
- // forward left and right
-  if (Input.includes("KeyY") && (oriantation === 90 || oriantation === -270)){
-   startX += fullAhead;
-  }  
-  if (Input.includes("KeyY") && (oriantation === -90 || oriantation === 270)){
-   startX -= fullAhead;
-  }
- // forward in top diagonal
-  if (Input.includes("KeyY") && (oriantation === -45 || oriantation === 315)){
-   startY -= fullAhead;
-   startX -= fullAhead;
-  }
-  if (Input.includes("KeyY") && (oriantation === 45 || oriantation === -315)){
-   startY -= fullAhead;
-   startX += fullAhead;
-  }
-  //Forward (default fliped)
-  if (Input.includes("KeyY") && (oriantation === 180 || oriantation === -180)){
-   startY += fullAhead;
-  }
+const fullAhead = 0.005;
+const astern = 0.0025;
+//Functions 
+ function lineX (){
+ return newX - currX; 
 }
 
-function backwards () {// should forward be like this 
- //reverse (default)
-  if (oriantation === 0){
-   startY += astern;
+function lineY (){ 
+ return newY - currY; 
+
+/* if (Input.includes("KeyY") && deg === 15 || deg === -345){ 
+  return (lineX () * (2-Math.sqrt(3)));
   }
- //reverse in bottom diagonal
-  if (oriantation === 135 || oriantation === -225){
-   startY -= astern;
-   startX -= astern;
-  }
-    if (oriantation === -135 || oriantation === 225){
-   startY -= astern;
-   startX += astern;
-  }
- //reverse left and right
-  if (oriantation === -90 || oriantation === 270){
-   startX += astern;
-  }
-  if (oriantation === 90 || oriantation === -270){
-   startX -= astern;
-  }
- //reverse in top diagonal
-  if (oriantation === 45 || oriantation === -315){
-   startY += astern;
-   startX -= astern;
-  }
-    if (oriantation === -45 || oriantation === 315){
-   startY += astern;
-   startX += astern;
-  }
- //reverse (default fliped)
-  if (oriantation === 180 || oriantation === -180){
-   startY -= astern;
-  }
+  */
 }
-/* 
-function updateWindow() {
-  return window.innerHeight,window.innerWidth;
+
+
+
+function move (start, line, M){
+ return start + line * M;
 }
- */
+function forward (){
+if ( deg === 0){
+ newX += fullAhead; 
+}
+
+ if (deg === 15 || deg === -345){
+ newX += fullAhead;
+
+} 
+
+if ( deg === -90 || deg === 270){
+ newY -= fullAhead;
+}
+
+if ( deg === 90 || deg === -270){
+ newY += fullAhead;
+}
+
+if ( Math.abs(deg) === 180){
+ newX -= fullAhead;
+}
+
+}
+
+function backward (){
+ if (deg === 0) {
+  newX -= astern;
+ }
+}
+
 Util.setSize(size,width);
 // Code that runs over and over again
 function loop() {
- //updateWindow() 
- Util.setPositionPixels(startX,startY);
-  console.log(`x: ${startX} y: ${startY}`);
- Util.setRotation(oriantation);
-  console.log(Input);
-  if (oriantation === 360 || oriantation === -360) {
-    oriantation = 0; 
+ if (Input.includes("KeyY")){
+ forward();
+ console.log("go");
+ }
+  if (Input.includes("KeyB")){
+ backward();
+ console.log("back");
+ }
+ console.log(deg);
+ if (Input.includes("KeyH")){
+  momentom = 0;
+ } else {
+  momentom = 0.01;
+ }
+  currX = move(currX,lineX(), momentom);
+  currY = move(currY,lineY(), momentom);
+ Util.setPosition(currX,currY);
+
+console.log(`x: ${currX*innerWidth} , y: ${currY*innerHeight}`)
+  if (deg === 360 || deg === -360) {
+    deg = 0; 
   }
-  console.log(oriantation);
-  forward();
+ Util.setRotation(deg);
+ /* 
+
+
   if (Input[0] === "KeyB" && Input[1] === "KeyN"){
    backwards();
   }
-
+ */
+ //updateWindow() 
   window.requestAnimationFrame(loop);
 }
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
-  
   // Put your event listener code here
 document.addEventListener("keydown",(event) => {
-  if (!event.repeat){
+  if (!event.repeat){ //? still invanitly spins 
  Input.push (event.code); 
   }
   if (Input.includes("KeyT")) {//here so that it only runs once per button press
-   oriantation -= 45;  
+   deg -= 15;  
   }
   if (Input.includes("KeyU")) {//here so that it only runs once per button press
-   oriantation += 45;  
+   deg += 15;  
   }
 });
 document.addEventListener("keyup",(event) => {
   Input.splice (Input.indexOf(event.code), 1 );
+  
 });
+
   window.requestAnimationFrame(loop);
 }
 
